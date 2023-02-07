8a2312aa11dd61e9059fe3796537e1d494ee784e
GuVictory <gubanova.vi10@gmail.com>
2023-02-07 11:57:37 +0300

    logger added

---
+++ src/utils/logger.ts
@@ -0,0 +1,62 @@
+import winston from 'winston';
+
+const customFormat = winston.format.printf(({ level, message, timestamp }) => {
+    return `${timestamp} [${level.toLocaleUpperCase()}]: ${JSON.stringify(message)}`;
+});
+
+export class Logger {
+    private static instance: winston.Logger;
+
+    static get i(): winston.Logger {
+        if (Logger.instance === undefined) {
+            Logger.instance = winston.createLogger({
+                level: 'info',
+                format: winston.format.combine(
+                    winston.format.json(),
+                    winston.format.timestamp({
+                        format: 'MMM-DD-YYYY HH:mm:ss',
+                    }),
+                    customFormat,
+                ),
+                transports: [
+                    new winston.transports.Console({
+                        level: 'debug',
+                        format: winston.format.combine(
+                            winston.format.json(),
+                            winston.format.timestamp({
+                                format: 'MMM-DD-YYYY HH:mm:ss',
+                            }),
+                            winston.format.prettyPrint(),
+                        ),
+                    }),
+                ],
+            });
+
+            if (process.env.NODE_ENV !== 'production') {
+                Logger.instance.add(
+                    new winston.transports.Console({
+                        format: winston.format.combine(
+                            winston.format.json(),
+                            winston.format.timestamp({
+                                format: 'MMM-DD-YYYY HH:mm:ss',
+                            }),
+                            winston.format.prettyPrint(),
+                        ),
+                    }),
+                );
+            }
+
+            if (process.env.NODE_ENV === 'production') {
+                Logger.instance.add(
+                    new winston.transports.File({ filename: 'logs/errors.log', level: 'error' }),
+                );
+                Logger.instance.add(new winston.transports.File({ filename: 'logs/combined.log' }));
+            }
+        }
+        return Logger.instance;
+    }
+
+    private constructor() {
+        // Do nothing
+    }
+}
